4b59e0dbcc364a841d8131b9f8c5ae4526a33927
Dusty4500 <91434463+Dusty4500@users.noreply.github.com>
2024-04-29 22:29:18 -0600

    Completed

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿Board board = new Board();
+
+do {
+    board.Display();
+
+    board.SelectPiece();
+} while(board.GameContinue);
+
+if (board.Player == "Red") {
+    board.Player = "Blue";
+} else {
+    board.Player = "Red";
+}
+
+board.Display();
+
+Console.WriteLine($"{board.Player} wins!");
